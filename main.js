0296967f68572a1ff5b82f8c7d626a6d7bae0e4c
Jon Howard <johnny.rtn@gmail.com>
2025-02-20 04:35:05 -0330

    Implement state machine using xstate in main.js

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './style.css';
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from 'xstate';
+
+const machine = createMachine({
+  initial: 'loading',
+  states: {
+    loading: {
+      on: {
+        SUCCESS: 'loaded',
+      },
+    },
+    loaded: {},
+  },
+});
+
+const service = interpret(machine).start();
+
+service.subscribe((state) => {
+  console.log(state.value);
+});
+
+window.service = service;
