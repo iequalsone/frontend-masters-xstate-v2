02892473cb7e61802917e65f982a3659e0e3fd33
Jon Howard <johnny.rtn@gmail.com>
2025-02-23 06:58:45 -0330

    Refactor player state machine to use parallel states and restructure volume management

--- 06-parallel-states/main.js
+++ 06-parallel-states/main.js
@@ -23,76 +23,86 @@ const playerMachine = createMachine({
   //   player: 'loading',
   //   volume: 'muted'
   // }
-  initial: 'loading',
+  type: 'parallel',
   states: {
-    // These states should be in a parent 'player' region
-    loading: {
-      id: 'loading',
-      tags: ['loading'],
-      on: {
-        LOADED: {
-          actions: 'assignSongData',
-          target: 'ready',
-        },
-      },
-    },
-    ready: {
-      initial: 'playing',
+    player: {
+      initial: 'loading',
       states: {
-        paused: {
+        // These states should be in a parent 'player' region
+        loading: {
+          id: 'loading',
+          tags: ['loading'],
           on: {
-            PLAY: { target: 'playing' },
+            LOADED: {
+              actions: 'assignSongData',
+              target: 'ready',
+            },
           },
         },
-        playing: {
-          entry: 'playAudio',
-          exit: 'pauseAudio',
-          on: {
-            PAUSE: { target: 'paused' },
-          },
-          always: {
-            cond: (ctx) => ctx.elapsed >= ctx.duration,
-            target: '#loading',
+        ready: {
+          initial: 'playing',
+          states: {
+            paused: {
+              on: {
+                PLAY: { target: 'playing' },
+              },
+            },
+            playing: {
+              entry: 'playAudio',
+              exit: 'pauseAudio',
+              on: {
+                PAUSE: { target: 'paused' },
+              },
+              always: {
+                cond: (ctx) => ctx.elapsed >= ctx.duration,
+                target: '#loading',
+              },
+            },
           },
         },
       },
-    },
-
-    // These states should be in a parent 'volume' region
-    unmuted: {
       on: {
-        'VOLUME.TOGGLE': 'muted',
+        // These should belong to the 'player' region
+        SKIP: {
+          actions: 'skipSong',
+          target: '#loading',
+        },
+        LIKE: {
+          actions: 'likeSong',
+        },
+        UNLIKE: {
+          actions: 'unlikeSong',
+        },
+        DISLIKE: {
+          actions: ['dislikeSong', raise('SKIP')],
+        },
+        'AUDIO.TIME': {
+          actions: 'assignTime',
+        },
       },
     },
-    muted: {
-      on: {
-        'VOLUME.TOGGLE': 'unmuted',
+    volume: {
+      initial: 'unmuted',
+      states: {
+        // These states should be in a parent 'volume' region
+        unmuted: {
+          on: {
+            'VOLUME.TOGGLE': 'muted',
+          },
+        },
+        muted: {
+          on: {
+            'VOLUME.TOGGLE': 'unmuted',
+          },
+        },
       },
-    },
-  },
-  on: {
-    // These should belong to the 'player' region
-    SKIP: {
-      actions: 'skipSong',
-      target: '#loading',
-    },
-    LIKE: {
-      actions: 'likeSong',
-    },
-    UNLIKE: {
-      actions: 'unlikeSong',
-    },
-    DISLIKE: {
-      actions: ['dislikeSong', raise('SKIP')],
-    },
-    'AUDIO.TIME': {
-      actions: 'assignTime',
-    },
-
-    // This should belong to the 'volume' region
-    VOLUME: {
-      cond: 'volumeWithinRange',
-      actions: 'assignVolume',
+      on: {
+        // This should belong to the 'volume' region
+        VOLUME: {
+          cond: 'volumeWithinRange',
+          actions: 'assignVolume',
+        },
+      },
     },
   },
 }).withConfig({
